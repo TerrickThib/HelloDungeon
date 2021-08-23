d2eb0d489a12d7ee554ad4859b7d178316cb7840
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-08-23 12:07:03 -0500

    i Fixed my function so it works.
    make sure to at the end of the function to return inputReceived.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -6,152 +6,209 @@ namespace HelloDungeon
 {
     class Game
     {
+
+        //varibles  
+        string name = "Empty";
+        string characterClass = "";
+        int health = 100;
+        float power = 0;
+        bool gameOver = false;
+        int poison = -5;
+        string input;
+        bool validInputReceived = false;
+
+        //Fuction for Stats To display
+        void DisplayStats()
+        {
+            Console.WriteLine();
+            Console.WriteLine(name + " Stats:");
+            Console.WriteLine("Class:" + characterClass);
+            Console.WriteLine("Health:" + health);
+            Console.WriteLine("Power:" + power);
+        }
+
+        //Class selection Function
+        int GetInput(string description, string option1, string option2)
+        {
+            string input = "";
+            int inputReceived = 0;
+
+            while (inputReceived != 1 && inputReceived != 2)
+            {
+                //Selection Screen
+                Console.WriteLine(description);
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
+                Console.Write("> ");
+
+                //Get input from player
+                input = Console.ReadLine();
+
+                //If class pick is knight            
+                if (input == "1" || input == option1)
+                {
+                    //set input recived to the first option
+                    inputReceived = 1;
+                }
+
+                //If class pick is Wizard
+                else if (input == "2" || input == option2)
+                {
+                    //set input recived to the second option
+                    inputReceived = 2;
+                }
+                //If neither are true
+                else
+                {
+                    //display error
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey();
+                }
+
+                //so player can read what happened then clear
+                Console.ReadKey();
+                Console.Clear();
+            }
+            return inputReceived;
+        }
+
         public void Run()
         {
             //Start Screen
             string start = "Welcome To The Grungen";
-           Console.WriteLine(start);
-           
+            Console.WriteLine(start);
+
             //Name Exercise
-            string name = "Empty";
+
             Console.WriteLine("Please enter your name.");
             name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
 
-            //varibles                      
-            int currentArea = 1;
-            string characterClass = "";
-            int health = 100;           
-            float power = 0;
-            bool gameOver = false;
-
-
-            //Fuction for Stats To display
-            void DisplayStats()
-            {
-                Console.WriteLine(name + "stats:");
-                Console.WriteLine("Class:" + characterClass);
-                Console.WriteLine("Power" + power);
-            }
-            
-            int GetInput(string description, string option1, string option2)
-            {
-
-            }
-            string input = "";
-            bool validInputReceived = false;
-
-            while (validInputReceived == false)
-               
-            {
-                //Class selection
-                Console.WriteLine("Select a Class");
-                Console.WriteLine("1.Knight");
-                Console.WriteLine("2.Wizard");
-                input = Console.ReadLine();
-                
-                //If class pick is knight            
-                if (input == "1" || input == "Knight")
-                {
-                    characterClass = "Knight";
-                    health = 100;
-                    power = 10;
-                    validInputReceived = true;
-                }
-
-                //If class pick is Wizard
-                else if (input == "2" || input == "Wizard")
-                {
-                    characterClass = "Wizard";
-                    health = 75;
-                    power = 15;
-                    validInputReceived = true;
-                }
-                //If neither are true
-               else
-               {
-                    Console.WriteLine("Invalid Input"); 
-               }
-                
-                Console.ReadKey();
-                Console.Clear();
-            }
-            Console.WriteLine("Your Characters Stats\n");
-            Console.WriteLine("Name " + name);
-            Console.WriteLine("Class " + characterClass);
-            Console.WriteLine("Health " + health);
-            Console.WriteLine("Power " + power);
-          
-            //Number of attempts a Loop
-            int numberOfAttempts = 4;
-
-            Console.WriteLine("A very old man with a monkey on his back approches you." + "\n The monkey offers you imortality if you can solve a riddle in " + numberOfAttempts);
             Console.ReadKey();
+            Console.Clear();
 
-            for (int i = 0; i < numberOfAttempts; i++)
+            //Calling Class Selection
+           int Input = GetInput("Select a Class", "Knight", "Wizard");
+
+            //If class pick is knight            
+            if (Input == 1)
             {
-                Console.Clear();
-                Console.WriteLine("What has to be broken befor you can use it?");
-                int attemptsRemaining = numberOfAttempts - i;
-                Console.WriteLine("Attempts Remaining: " + attemptsRemaining);
-                Console.Write("> ");
-                input = Console.ReadLine();
-
-                if (input == "egg")
-                {
-                    Console.WriteLine("Congrates! You've gained immortality!");
-                    break;
-                }
-                Console.WriteLine("Incorrect! The monkey laughs at you! It hurts...." + "\n you take 5 points of damage.");
-                health -= 5;                
+                characterClass = "Knight";
+                health = 100;
+                power = 10;
+                validInputReceived = true;
             }
-            Console.WriteLine($"Your Hp: {health}");
 
-            //First Choice
+            //If class pick is Wizard
+            else if (Input == 2)
+            {
+                characterClass = "Wizard";
+                health = 75;
+                power = 15;
+                validInputReceived = true;
+            }
+            DisplayStats();
+
+            //Man offers you a potion
             validInputReceived = false;
             while (validInputReceived == false)
-            {                                   
-                Console.WriteLine("You approach a cave. " + "\nDo you go into the cave? " + "\n1.Yes \n2.No");
-                input = Console.ReadLine();
+            {
+                Console.WriteLine("Your aproached by a traveler. " +
+                    "\n They offer you a potion. Do you accept?", "\n 1.yes", "\n 2.no");
 
-                if (input == "1" || input == "Yes")
+                input = Console.ReadLine();
+                if (input == "1" || input == "yes")
                 {
+                    Console.WriteLine("It was posion!" + "\n You lose 5 health");
+                    Console.WriteLine(health += poison);
                     validInputReceived = true;
-                    Console.WriteLine("You enter the cave. Wow you are brave.");
-                    
                 }
 
-                else if (input == "2" || input == "No")
+                else if (input == "2" || input == "no")
                 {
+                    Console.WriteLine("You decline his offer and move on.");
                     validInputReceived = true;
-                    Console.WriteLine("You try turning around and going to leave, but you trip and fall in anyway. Nice try.");
-                    
                 }
 
                 else
                 {
                     Console.WriteLine("Invalid Choice");
                 }
-             }
 
-            //Second Encounter
-            {
-                Console.WriteLine("Now that you are in the cave you hear the sound of rocks gringing . You look where you just entered from to see the faint light disapear.");
-                Console.WriteLine("Your stuck in the cave with only one way to go, so you go deeper into the cave. ");
+
+
+
+                //Number of attempts a Loop
+                int numberOfAttempts = 4;
+
+                Console.WriteLine("A very old man with a monkey on his back approches you." + "\n The monkey offers you imortality if you can solve a riddle in " + numberOfAttempts);
+                Console.ReadKey();
+
+                for (int i = 0; i < numberOfAttempts; i++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("What has to be broken befor you can use it?");
+                    int attemptsRemaining = numberOfAttempts - i;
+                    Console.WriteLine("Attempts Remaining: " + attemptsRemaining);
+                    Console.Write("> ");
+                    input = Console.ReadLine();
+
+                    if (input == "egg")
+                    {
+                        Console.WriteLine("Congrates! You've gained immortality!");
+                        break;
+                    }
+                    Console.WriteLine("Incorrect! The monkey laughs at you! It hurts...." + "\n you take 5 points of damage.");
+                    health -= 5;
+                }
+                Console.WriteLine($"Your Hp: {health}");
+
+                //First Choice
+                validInputReceived = false;
+                while (validInputReceived == false)
+                {
+                    Console.WriteLine("You approach a cave. " + "\nDo you go into the cave? " + "\n1.Yes \n2.No");
+                    input = Console.ReadLine();
+
+                    if (input == "1" || input == "yes")
+                    {
+                        validInputReceived = true;
+                        Console.WriteLine("You enter the cave. Wow you are brave.");
+
+                    }
+
+                    else if (input == "2" || input == "no")
+                    {
+                        validInputReceived = true;
+                        Console.WriteLine("You try turning around and going to leave, but you trip and fall in anyway. Nice try.");
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Invalid Choice");
+                    }
+                }
+
+                //Second Encounter
+                {
+                    Console.WriteLine("While in the cave you hear the sound of rocks gringing . You look where you just entered from to see the faint light disapear.");
+                    Console.WriteLine("Your stuck in the cave with only one way to go, so you go deeper into the cave. ");
+
+                }
+
+
+
+
+
+
+
+
+
+
+
 
             }
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
 }
